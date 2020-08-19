f28e3afec470f2fa0c022f984680a9101b2d7be0
pplitzner <p.plitzner@posteo.de>
2020-08-19 09:12:48 +0200

    Add dummy data to startup run() method

--- src/main/java/de/cpht/haushalter/HaushalterApplication.java
+++ src/main/java/de/cpht/haushalter/HaushalterApplication.java
@@ -1,13 +1,37 @@
 package de.cpht.haushalter;
 
+import de.cpht.haushalter.model.Plan;
+import de.cpht.haushalter.model.PlanItem;
+import de.cpht.haushalter.repository.PlanItemRepository;
+import de.cpht.haushalter.repository.PlanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HaushalterApplication {
+public class HaushalterApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HaushalterApplication.class, args);
 	}
 
+	@Autowired
+	private PlanRepository planRepository;
+	@Autowired
+	private PlanItemRepository itemRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Plan plan = new Plan();
+		plan.setTitle("Küche");
+		plan.setDescription("Erst die Oberflächen!");
+		planRepository.save(plan);
+
+		PlanItem item = new PlanItem();
+		item.setTitle("Spülmaschine ausräumen");
+		item.setDescription("Becher unten trocknen");
+		item.setPlan(plan);
+		itemRepository.save(item);
+	}
 }
