e65dad58e6f197fc46c5f5d8bad0e1f948f580ab
pplitzner <p.plitzner@posteo.de>
2020-08-30 17:43:58 +0200

    Add plan item REST controller

---
+++ src/main/java/de/cpht/haushalter/controller/PlanItemRESTController.java
@@ -0,0 +1,43 @@
+package de.cpht.haushalter.controller;
+
+import de.cpht.haushalter.domain.entities.Plan;
+import de.cpht.haushalter.domain.entities.PlanItem;
+import de.cpht.haushalter.domain.usecases.PlanUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("plans/{planId}/items")
+public class PlanItemRESTController {
+
+    @Autowired
+    private PlanUseCase planUseCase;
+
+    @GetMapping
+    public List<PlanItem> index(@PathVariable Long planId){
+        return planUseCase.getItems(planId);
+    }
+
+//    @PostMapping
+//    public Long store(@RequestBody Plan plan) {
+//        return planUseCase.startPlan(plan.title, plan.description);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public Plan show(@PathVariable Long id){
+//        return planUseCase.getPlanById(id);
+//    }
+//
+//    @PostMapping("/{id}")
+//    public void update(@PathVariable Long id, @RequestBody Plan updatedPlan){
+//        planUseCase.updatePlan(id, updatedPlan);
+//    }
+//
+//    @DeleteMapping("{id}")
+//    public void delete(@PathVariable Long id){
+//        planUseCase.deletePlan(id);
+//    }
+}
