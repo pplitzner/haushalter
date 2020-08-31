34a8bc8714c944fdf1ebf7bc19420f266736bc55
pplitzner <p.plitzner@posteo.de>
2020-08-31 08:06:54 +0200

    Organize imports

--- src/main/java/de/cpht/haushalter/controller/PlanRESTController.java
+++ src/main/java/de/cpht/haushalter/controller/PlanRESTController.java
@@ -3,7 +3,6 @@ package de.cpht.haushalter.controller;
 import de.cpht.haushalter.domain.entities.Plan;
 import de.cpht.haushalter.domain.usecases.PlanUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
