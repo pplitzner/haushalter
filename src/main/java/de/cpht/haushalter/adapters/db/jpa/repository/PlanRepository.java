1b11caf07347569dbfa9b00a4a6f33d97ac11955
pplitzner <p.plitzner@posteo.de>
2020-10-15 12:58:44 +0200

    Remove redundant visibility

--- src/main/java/de/cpht/haushalter/adapters/db/jpa/repository/PlanRepository.java
+++ src/main/java/de/cpht/haushalter/adapters/db/jpa/repository/PlanRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PlanRepository extends JpaRepository<JpaPlan, Long> {
-    public List<JpaPlan> findByIsDefault(boolean isDefault);
+    List<JpaPlan> findByIsDefault(boolean isDefault);
 }
