2e57fd115ae36a20519c944a4561195856ea9582
pplitzner <p.plitzner@posteo.de>
2020-10-15 07:19:22 +0200

    Add dev profile to http tracing bean

--- src/main/java/de/cpht/haushalter/HaushalterConfig.java
+++ src/main/java/de/cpht/haushalter/HaushalterConfig.java
@@ -4,7 +4,9 @@ import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("dev")
 @Configuration
 public class HaushalterConfig {
     @Bean
