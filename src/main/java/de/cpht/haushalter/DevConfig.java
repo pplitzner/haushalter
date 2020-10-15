ebb408ecdc5ebbeb2eb495a32b3dc17791853926
pplitzner <p.plitzner@posteo.de>
2020-10-15 14:38:16 +0200

    Enable scheduling + rename dev config

--- src/main/java/de/cpht/haushalter/DevConfig.java
+++ src/main/java/de/cpht/haushalter/DevConfig.java
@@ -5,10 +5,11 @@ import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Profile("dev")
 @Configuration
-public class HaushalterConfig {
+public class DevConfig {
     @Bean
     public HttpTraceRepository htttpTraceRepository()
     {
