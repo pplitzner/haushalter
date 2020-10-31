95dc2f5cabdae7cd3b6d338e59ab01b9dd0baabd
pplitzner <p.plitzner@posteo.de>
2020-10-31 15:58:32 +0100

    Add commented startup dummy code

--- src/main/java/de/cpht/haushalter/HaushalterApplication.java
+++ src/main/java/de/cpht/haushalter/HaushalterApplication.java
@@ -1,12 +1,22 @@
 package de.cpht.haushalter;
 
+import de.cpht.haushalter.adapters.db.jpa.entity.JpaPlan;
+import de.cpht.haushalter.adapters.db.jpa.entity.JpaPlanItem;
+import de.cpht.haushalter.adapters.db.jpa.repository.PlanItemRepository;
+import de.cpht.haushalter.adapters.db.jpa.repository.PlanRepository;
+import de.cpht.haushalter.domain.entities.PlanType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 @EnableScheduling
-public class HaushalterApplication {//implements CommandLineRunner {
+public class HaushalterApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HaushalterApplication.class, args);
@@ -16,9 +26,9 @@ public class HaushalterApplication {//implements CommandLineRunner {
 //	private PlanRepository planRepository;
 //	@Autowired
 //	private PlanItemRepository itemRepository;
-//
-//	@Override
-//	public void run(String... args) throws Exception {
+
+	@Override
+	public void run(String... args) throws Exception {
 //		JpaPlan plan = new JpaPlan();
 //		plan.setTitle("Küche");
 //		plan.setDescription("Erst die Oberflächen!");
@@ -28,7 +38,9 @@ public class HaushalterApplication {//implements CommandLineRunner {
 //		JpaPlanItem item = new JpaPlanItem();
 //		item.setTitle("Spülmaschine ausräumen");
 //		item.setDescription("Becher unten trocknen");
+//		item.setDuration(Duration.ofMinutes(15));
+//		item.setCheckedAt(LocalDateTime.now().minusDays(1));
 //		item.setPlan(plan);
 //		itemRepository.save(item);
-//	}
+	}
 }
