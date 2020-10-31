43b147396d0037fec46eb06e2914f3d8566f06e8
pplitzner <p.plitzner@posteo.de>
2020-10-31 09:21:06 +0100

    Add REST controller + endpoint for getUncheckedItems()

---
+++ src/main/java/de/cpht/haushalter/controller/ItemRESTController.java
@@ -0,0 +1,24 @@
+package de.cpht.haushalter.controller;
+
+import de.cpht.haushalter.domain.entities.PlanItem;
+import de.cpht.haushalter.domain.usecases.PlanUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
+public class ItemRESTController {
+
+    @Autowired
+    private PlanUseCase planUseCase;
+
+    @GetMapping("todo")
+    public List<PlanItem> getTodos(){
+        return planUseCase.getUncheckedItems();
+    }
+
+}
